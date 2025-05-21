389a7b0ea33376d7a536b15f2b66a6f3318d0a10
nidhinagpure <nagpurenidhi8@gmail.com>
2025-05-21 23:13:24 +0530

    add view pages

--- src/componet/headsection/Headsection.jsx
+++ src/componet/headsection/Headsection.jsx
@@ -1,52 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import './../../componet/button/Button.css';
 import './../../componet/headsection/Headsection.css';
-import studyvideo from './../../images/headersection/video-lesson.png';
 
-   export function Headsection(props){
-    const {image , title , heaidng} = props;
-    return(
 
+export function Headsection() {
+  return (
       <>
+        <div>
+          <span className="first-title">Quality courses at affordable pricing, <br></br>trusted by 2000+ students.</span><br></br><br></br>
+          <span className="sec-title">We help you to learn the latest technologies and tools to kickstart your career<br></br> in tech.</span><br></br><br></br>
+          <span className="thrid-title">Learn, Practice, Master and Achieve.</span>
+        </div>
+        <div className='header-button'>
+          <button className="action-button header-btn">Courses</button>
+          <button className="action-button header-btn">Login</button>
+        </div>
+    </>
+  );
+}
 
-      <div>
-
-      <div className='headerdiv'>
-       <span className="first-title">Quality courses at affordable pricing, <br></br>trusted by 2000+ students.</span><br></br><br></br>
-       <span className="sec-title">We help you to learn the latest technologies and tools to kickstart your career<br></br> in tech.</span><br></br><br></br>
-       <span className="thrid-title">Learn, Practice, Master and Achieve.</span>
-      </div>
-       <div className='header-button'>
-       <button className="action-button header-btn">Courses</button> 
-       <button className="action-button header-btn">Login</button>
-       </div>
-       
-            
+export function Headerdiv(props){
+    const { image , title , heading } = props;
+    return(
       <div className="headsection-container">
-        <div className="">
-
-          <div>
-           <img src={ studyvideo } className="head-image"></img>
-          </div>
-
-           <span className="head-title">Enroll and get started.</span><br></br>
-           <span className="head-heading">Join live interactive classes and learn from the best instructors.</span>
-      </div>
-
-      </div>      
-
-       
-      </div>
-
       
+      <div className="header-box">
 
-      </>
+         <img src={image} className="head-image"></img>
+
+
+        <div className='header-lines'>
+         <span className="head-title">{title}</span><br></br>
+         <span className="head-heading">{heading}</span>
+        </div>
+    </div>
+
+    </div>  
     );
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-  <>
-   <Headsection />
-  </>
-)
--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import './../home/Home.css';
+
 import { Navbar } from './../../componet/navbar/Navbarr';
 import { Headsection }  from './../../componet/headsection/Headsection';
-import { Button } from './../../componet/button/Button';
+import { Headerdiv } from './../../componet/headsection/Headsection';
+
+import Video from './../../images/headersection/video-lesson.png';
+import Practice from './../../images/headersection/practice.webp';
+import Trophy from './../../images/headersection/trophy.webp';
 
 const HomePage = () => {
   return (
     <div style={{ padding: '2rem' }}>
       <Navbar />
-      <div>
 
-        <div>
-          <Headsection />
-          <Button />
+      <div className="box-container">
+        <div className="">
+           <Headsection/>
+        </div>   
+        <div className="">        
+          <Headerdiv image={ Video } title="Enroll and get started." heading="Join live interactive classes and learn from the best instructors." /><br></br>
+          <Headerdiv image={ Practice } title="Learn, Practice, and Master." heading="Learn, resolve doubts, and practice with real-world projects."/><br></br>
+          <Headerdiv image={ Trophy }title="Grab the opportunity." heading="Apply for internships and jobs with the skills you have learned."/><br></br>
         </div>
 
-        <div>
-          
-        
-
-
-        </div>
       </div>
       
     </div>
