7c2d90e44849da165cb0b447454c8c01f7be70f6
nidhinagpure <nagpurenidhi8@gmail.com>
2025-05-22 07:29:48 +0530

    navbar headersection responsive

--- src/componet/headsection/Headsection.jsx
+++ src/componet/headsection/Headsection.jsx
@@ -6,7 +6,7 @@ import './../../componet/headsection/Headsection.css';
 export function Headsection() {
   return (
       <>
-        <div>
+        <div className='header-div'>
           <span className="first-title">Quality courses at affordable pricing, <br></br>trusted by 2000+ students.</span><br></br><br></br>
           <span className="sec-title">We help you to learn the latest technologies and tools to kickstart your career<br></br> in tech.</span><br></br><br></br>
           <span className="thrid-title">Learn, Practice, Master and Achieve.</span>
@@ -22,8 +22,7 @@ export function Headsection() {
 export function Headerdiv(props){
     const { image , title , heading } = props;
     return(
-      <div className="headsection-container">
-      
+ 
       <div className="header-box">
 
          <img src={image} className="head-image"></img>
@@ -33,8 +32,7 @@ export function Headerdiv(props){
          <span className="head-title">{title}</span><br></br>
          <span className="head-heading">{heading}</span>
         </div>
-    </div>
-
+   
     </div>  
     );
 }
